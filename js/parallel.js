382eaa2dda4a72390caa882c8cb531807cb4dc62
Kinshuk <kinshuksinha91@yahoo.co.in>
2016-05-04 10:15:01 -0700

    parallel done

--- js/parallel.js
+++ js/parallel.js
@@ -3,30 +3,34 @@ var margin = {top: 30, right: 100, bottom: 20, left: 280},
     width = 1100 - margin.left - margin.right,
     height = 500 - margin.top - margin.bottom;
 
-var dset = "../data/parallel2013.csv";
+var dset = "../data/parallel2011.csv";
 startD3();
-// var yearSelector = function (event) {
-// 	var element = document.getElementById("year");
-// 	var selectedValue = element.options[element.selectedIndex].text;
+var yearSelector = function (event) {
+	var element = document.getElementById("year");
+	var selectedValue = element.options[element.selectedIndex].text;
     
-//     if (selectedValue == "2016") {
-//     	dset = "timesData2016.csv";
-//     	startD3();
-//     }
-//     else if (selectedValue =="2015") {
-//     	dset = "timesData2015.csv";
-//     	startD3();
-//     }
-//     else if (selectedValue =="2014") {
-//     	dset = "timesData2014.csv";
-//     	startD3();
-//     }
-//     else if (selectedValue =="2013") {
-//     	dset = "timesData2013.csv";
-//     	startD3();
-//     }
-//     console.log(dset);
-// };
+    if (selectedValue == "2011") {
+    	dset = "../data/parallel2011.csv";
+    	startD3();
+    }
+    else if (selectedValue =="2012") {
+    	dset = "../data/parallel2012.csv";
+    	startD3();
+    }
+    else if (selectedValue =="2013") {
+    	dset = "../data/parallel2013.csv";
+    	startD3();
+    }
+    else if (selectedValue =="2014") {
+    	dset = "../data/parallel2014.csv";
+    	startD3();
+    }
+    else if (selectedValue =="2015") {
+      dset = "../data/parallel2015.csv";
+      startD3();
+    }
+    console.log(dset);
+};
 
 function startD3(){
 	console.log("entereing main");
